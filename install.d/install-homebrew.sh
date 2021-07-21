9aeaf832da8e540e82d421c34f24f5113db08e01
Gordon Chiam <gordon.chiam@gmail.com>
2021-07-21 07:10:34 +0000

    Install homebrew

---
+++ install.d/install-homebrew.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
+./install.d/install-core.sh
+. ./install.d/install-homebrew.sh
+./install.d/install-homebrew-gcc.sh
 ./install.d/install-neovim.sh
