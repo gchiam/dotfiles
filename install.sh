c114c9f313ecd7d3f4d7ce8e539f39b21610abcd
Gordon Chiam <gordon.chiam@gmail.com>
2021-07-21 07:12:26 +0000

    Install git-delta

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ sudo apt upgrade -y
 ./install.d/install-core.sh
 . ./install.d/install-homebrew.sh
 ./install.d/install-homebrew-gcc.sh
+./install.d/install-homebrew-git-delta.sh
 ./install.d/install-neovim.sh
