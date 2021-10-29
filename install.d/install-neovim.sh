bb3489e409cd7857f0166abc26ebb5ea6662104f
Gordon Chiam <gordon.chiam@gmail.com>
2021-10-29 16:48:45 +0000

    Install daily built of neovim

--- install.d/install-neovim.sh
+++ install.d/install-neovim.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-sudo apt-get install -y neovim python3-neovim
+mkdir -p ~/.local/opt/nvim
+cd ~/.local/opt/nvim
+curl -sLO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod +x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+mkdir -p ~/.local/bin
+ln -snvf ~/.local/opt/nvim/squashfs-root/usr/bin/nvim ~/.local/bin/nvim
+
+pip install -U pip
+pip install neovim
+cd -
